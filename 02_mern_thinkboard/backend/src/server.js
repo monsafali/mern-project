f7689c25fb7a5e385147aea978f9648026a7227c
monsafali <monsafali272@gmail.com>
2025-06-05 12:04:10 +0500

    something new frontend feature added

--- 02_mern_thinkboard/backend/src/server.js
+++ 02_mern_thinkboard/backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 
 import notesRoutes from "./routes/notesRoutes.js";
@@ -10,16 +11,16 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); // this middleware will parse JSON bodies: req.body
-// our simple custom middleware
-// app.use((req, res, next) => {
-//   console.log(`REq path is ${req.path}, and req methoed is ${req.method}`);
-//   console.log("men pherser render hogya hon");
-//   next();
-// });
 
 app.use(rateLimiter);
-app.use("/api/notes", notesRoutes);
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
+app.use("/api/notes", notesRoutes);
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log("Server started on PORT:", PORT);
--- 02_mern_thinkboard/frontend/src/pages/HomePage.jsx
+++ 02_mern_thinkboard/frontend/src/pages/HomePage.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import RateLimitedUi from "../components/RateLimitedUi";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 function HomePage() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [isRateLimited, setIsRateLimited] = useState(true);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await axios.get("http://localhost:3000/api/notes");
+        setNotes(res.data);
+        setIsRateLimited(false);
+      } catch (error) {
+        console.log("Error in fettin data", error);
+        if (error.response?.status === 429) {
+          toast.error("Failed to load data");
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchNotes();
+  }, []);
+
+  return (
+    <h1 className="min-h-screen">
+      <Navbar />
+      {isRateLimited && <RateLimitedUi />}
+    </h1>
+  );
 }
 
 export default HomePage;
