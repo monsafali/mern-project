e8965d1ff68223ec7b06fb318bd96882801e62ea
“monsafali” <monsafali272@gmail.com>
2025-06-07 18:08:47 +0500

    nodemailer added

--- 03_Ai_Agent_Full_stack/backend/utils/mailer.js
+++ 03_Ai_Agent_Full_stack/backend/utils/mailer.js
@@ -1,1 +1,30 @@
-import nodemai
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const senMail = async (to, subject, text) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.MAILTRAP_SMTP_HOST,
+      port: process.env.MAILTRAP_SMTP_PORT,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: process.env.MAILTRAP_SMTP_USE,
+        pass: process.env.MAILTRAP_SMTP_PASS,
+      },
+    });
+
+    const info = await transporter.sendMail({
+      from: "monsaf ali",
+      to: to,
+      subject: subject,
+      text: text, // plain‑text body
+    });
+
+    console.log("Message sent:", info.messageId);
+    return info;
+  } catch (error) {
+    console.error("Mail Error", error.message);
+    throw error;
+  }
+};
