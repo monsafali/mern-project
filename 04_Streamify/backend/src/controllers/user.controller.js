9d6ccff7a62c185e37c7591874b34b8840484ad8
monsafali <monsafali272@gmail.com>
2025-06-15 17:18:09 +0500

    friend router added

--- 04_Streamify/backend/src/controllers/user.controller.js
+++ 04_Streamify/backend/src/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import FriendRequest from "../models/FriendRequest.js";
 import User from "../models/user.model.js";
 
 export async function getRecommendedUsers(req, res) {
@@ -34,6 +35,64 @@ export async function getMyFriends(req, res) {
       );
 
     res.status(200).json(user.friends);
+  } catch (error) {
+    res.status(200).json({
+      success: false,
+      message: "interval server Error ",
+    });
+  }
+}
+
+export async function sendFriendRequest(req, res) {
+  try {
+    const myId = req.user.id;
+    const { id: recipientId } = req.params;
+    // Prevent sending request ot your self
+    if (myId === recipientId) {
+      return res.status(400).json({
+        success: false,
+        message: "You can't send friend request to yourself",
+      });
+      const recipient = await User.findById(recipientId);
+      if (!recipient) {
+        return res.status(404).json({
+          success: false,
+          message: "Recipient user not found",
+        });
+      }
+      if (recipient.friends.includes(myId)) {
+        return res.status(400).json({
+          success: false,
+          message: "You are already friends with this user",
+        });
+      }
+
+      const existingRequest = await FriendRequest.findOne({
+        $or: [
+          { sender: myId, recipient: recipientId },
+          { sender: recipientId, recipient: myId },
+        ],
+      });
+
+      if (existingRequest) {
+        return res.status(400).json({
+          success: false,
+          message: "A freiend request already exist between your and this user",
+        });
+      }
+
+      const friendRequest = await FriendRequest.create({
+        sender: myId,
+        recipient: recipientId,
+      });
+
+      if (friendRequest) {
+        res.status(200).json({
+          success: true,
+          message: "Friend request sent successfully",
+        });
+      }
+    }
   } catch (error) {
     res.status(200).json({
       success: false,
--- 04_Streamify/backend/src/routers/user.route.js
+++ 04_Streamify/backend/src/routers/user.route.js
@@ -3,6 +3,7 @@ import { protectRoute } from "../middleware/auth.middleware.js";
 import {
   getMyFriends,
   getRecommendedUsers,
+  sendFriendRequest,
 } from "../controllers/user.controller.js";
 const router = express.Router();
 
@@ -10,4 +11,6 @@ const router = express.Router();
 router.use(protectRoute);
 router.get("/", protectRoute, getRecommendedUsers);
 router.get("/friends", protectRoute, getMyFriends);
+
+router.post("/friend-request/:id", protectRoute, sendFriendRequest);
 export default router;
