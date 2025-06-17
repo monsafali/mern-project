54a759255e171fcfc782e762f80d3d7074b161fb
“monsafali” <monsafali272@gmail.com>
2025-06-17 11:38:15 +0500

    signup route created

--- 04_Streamify/backend/src/server.js
+++ 04_Streamify/backend/src/server.js
@@ -1,9 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
-
 dotenv.config();
 import dbconnection from "./config/db.js";
+
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routers/auth.route.js";
 import userRoutes from "./routers/user.route.js";
@@ -12,6 +13,12 @@ import ChatRoutes from "./routers/chat.route.js";
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
--- 04_Streamify/frontend/src/main.jsx
+++ 04_Streamify/frontend/src/main.jsx
@@ -4,11 +4,16 @@ import App from "./App.jsx";
 import "./index.css";
 
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
 );
