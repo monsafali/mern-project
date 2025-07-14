272cdc089f63247b411ffa2848467a7f98fff8d4
“monsafali” <monsafali272@gmail.com>
2025-07-14 15:47:55 +0500

    getting message rout aded

--- 06_Chat_app_With_Context_Hook/Backend/Routes/message.routes.js
+++ 06_Chat_app_With_Context_Hook/Backend/Routes/message.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { sendMessage } from "../controllers/message.controller.js";
+import { GetMessage, sendMessage } from "../controllers/message.controller.js";
 import protectRoute from "../middleware/ProtectRoute.js";
 
 const MessageRouter = express.Router();
 
 MessageRouter.post("/send/:id", protectRoute, sendMessage);
+MessageRouter.get("/:id", protectRoute, GetMessage);
 
 export default MessageRouter;
